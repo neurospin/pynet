96f107c921260e233e91c1cb2dda5626f818b04f
Antoine Grigis <antoine.grigis@cea.fr>
2020-03-12 10:57:58 +0100

    pynet/datasets/core: pep8.

--- pynet/datasets/core.py
+++ pynet/datasets/core.py
@@ -30,13 +30,14 @@ class DataManager(object):
     """ Data manager used to split a dataset in train, test and validation
     pytorch datasets.
     """
-
     def __init__(self, input_path, metadata_path, output_path=None,
                  labels=None, stratify_label=None, custom_stratification=None,
-                 projection_labels=None, number_of_folds=10, batch_size=1, weighted_random_sampler=False,
-                 input_transforms=None, output_transforms=None, labels_transforms=None, stratify_label_transforms=None,
-                 data_augmentation=None, add_input=False, patch_size=None, input_size=None, test_size=0.1,
-                 **dataloader_kwargs):
+                 projection_labels=None, number_of_folds=10, batch_size=1,
+                 weighted_random_sampler=False, input_transforms=None,
+                 output_transforms=None, labels_transforms=None,
+                 stratify_label_transforms=None, data_augmentation=None,
+                 add_input=False, patch_size=None, input_size=None,
+                 test_size=0.1, **dataloader_kwargs):
         """ Splits an input numpy array using memory-mapping into three sets:
         test, train and validation. This function can stratify the data.
 
@@ -60,8 +61,8 @@ class DataManager(object):
             the name of the column in the metadata table containing the label
             used during the stratification.
         custom_stratification: dict, default None
-            same format as projection labels. It will split the dataset into train/test/val according
-            to the stratification defined in the dict.
+            same format as projection labels. It will split the dataset into
+            train/test/val according to the stratification defined in the dict.
         projection_labels: dict, default None
             selects only the data that match the conditions in the dict
             {<column_name>: <value>}.
@@ -70,11 +71,13 @@ class DataManager(object):
         batch_size: int, default 1
             the size of each mini-batch.
         weighted_random_sampler: bool, default False
-            Whether we use a weighted random sampler (to deal with imbalanced classes issue) or not
+            Whether we use a weighted random sampler (to deal with imbalanced
+            classes issue) or not
         input_transforms, output_transforms: list of callable, default None
             transforms a list of samples with pre-defined transformations.
         data_augmentation: list of callable, default None
-            transforms the training dataset input with pre-defined transformations on the fly during the training.
+            transforms the training dataset input with pre-defined
+            transformations on the fly during the training.
         add_input: bool, default False
             if true concatenate the input tensor to the output tensor.
         test_size: float, default 0.1
@@ -88,8 +91,9 @@ class DataManager(object):
 
         mask_indices = DataManager.get_indices_from_mask(mask)
 
-        # We should only work with masked data but we want to preserve the memory mapping so we are getting the right
-        # index at the end (in __getitem__ of ArrayDataset)
+        # We should only work with masked data but we want to preserve the
+        # memory mapping so we are getting the right index at the end
+        # (in __getitem__ of ArrayDataset)
 
         self.inputs = np.load(input_path, mmap_mode='r')
         self.outputs, self.labels, self.stratify_label = (None, None, None)
@@ -102,7 +106,8 @@ class DataManager(object):
 
         if stratify_label is not None:
             self.stratify_label = df[stratify_label].values
-            # Apply the labels transform here as a mapping to the integer representation of the classes
+            # Apply the labels transform here as a mapping to the integer
+            # representation of the classes
             for i in mask_indices:
                 label = self.stratify_label[i]
                 for tf in (stratify_label_transforms or []):
@@ -121,12 +126,15 @@ class DataManager(object):
 
         if self.weighted_random_sampler:
             if self.stratify_label is None:
-                raise ValueError('Impossible to use the WeightedRandomSampler if no stratify label is available.')
-            class_samples_count = [0 for _ in range(len(set(self.stratify_label[mask])))] # len == nb of classes
+                raise ValueError("Impossible to use the WeightedRandomSampler "
+                                 "if no stratify label is available.")
+            class_samples_count = [
+                0 for _ in range(len(set(self.stratify_label[mask])))]
             for label in self.stratify_label[mask]:
                 class_samples_count[label] += 1
             # Imbalanced weights in case of imbalanced classes issue
-            self.sampler_weigths = 1. / torch.tensor(class_samples_count, dtype=torch.float)
+            self.sampler_weigths = (
+                1. / torch.tensor(class_samples_count, dtype=torch.float))
 
         self.dataset = dict((key, [])
                             for key in ("train", "test", "validation"))
@@ -135,12 +143,16 @@ class DataManager(object):
         dummy_like_X_masked = np.ones(np.sum(mask))
         val_indices, train_indices, test_indices = (None, None, None)
         if custom_stratification is not None:
-            if "validation" in custom_stratification and stratify_label is not None:
-                print("Warning: impossible to stratify the data: validation+test set already defined ! ")
-            train_mask, test_mask = (DataManager.get_mask(df, custom_stratification["train"]),
-                                     DataManager.get_mask(df, custom_stratification["test"]))
+            if ("validation" in custom_stratification and
+                    stratify_label is not None):
+                print("Warning: impossible to stratify the data: "
+                      "validation+test set already defined ! ")
+            train_mask, test_mask = (
+                DataManager.get_mask(df, custom_stratification["train"]),
+                DataManager.get_mask(df, custom_stratification["test"]))
             if "validation" in custom_stratification:
-                val_mask = DataManager.get_mask(df, custom_stratification["validation"])
+                val_mask = DataManager.get_mask(
+                    df, custom_stratification["validation"])
                 val_mask &= mask
                 val_indices = DataManager.get_indices_from_mask(val_indices)
                 self.number_of_folds = 1
@@ -163,7 +175,8 @@ class DataManager(object):
             else:
                 splitter = ShuffleSplit(
                     n_splits=1, random_state=0, test_size=test_size)
-                train_indices, test_indices = next(splitter.split(dummy_like_X_masked))
+                train_indices, test_indices = next(
+                    splitter.split(dummy_like_X_masked))
                 train_indices = mask_indices[train_indices]
                 test_indices = mask_indices[test_indices]
         if self.labels is not None:
@@ -171,9 +184,9 @@ class DataManager(object):
         self.dataset["test"] = ArrayDataset(
             self.inputs, test_indices, labels=self.labels,
             outputs=self.outputs, add_input=self.add_input,
-            input_transforms = self.input_transforms,
-            output_transforms = self.output_transforms,
-            label_transforms = self.labels_transforms,
+            input_transforms=self.input_transforms,
+            output_transforms=self.output_transforms,
+            label_transforms=self.labels_transforms,
             patch_size=patch_size, input_size=input_size)
 
         if train_indices is None:
@@ -191,34 +204,39 @@ class DataManager(object):
             gen = kfold_splitter.split(
                 dummy_like_X_train,
                 np.array(self.stratify_label[train_indices], dtype=np.int32))
-            gen = [(train_indices[tr], train_indices[val]) for (tr, val) in gen]
+            gen = [(train_indices[tr], train_indices[val])
+                   for (tr, val) in gen]
 
         else:
             kfold_splitter = KFold(n_splits=number_of_folds)
             gen = kfold_splitter.split(dummy_like_X_train)
-            gen = [(train_indices[tr], train_indices[val]) for (tr, val) in gen]
+            gen = [(train_indices[tr], train_indices[val])
+                   for (tr, val) in gen]
 
         for fold_train_index, fold_val_index in gen:
             assert len(set(fold_val_index) & set(fold_train_index)) == 0
-            assert len(set(fold_val_index)) + len(set(fold_train_index)) == len(set(train_indices))
+            assert (len(set(fold_val_index)) + len(set(fold_train_index)) ==
+                    len(set(train_indices)))
             train_dataset = ArrayDataset(
                 self.inputs, fold_train_index,
                 labels=self.labels, outputs=self.outputs,
-                add_input=self.add_input, input_transforms=self.input_transforms+self.data_augmentation,
-                output_transforms=self.output_transforms+self.data_augmentation,
+                add_input=self.add_input,
+                input_transforms=self.input_transforms+self.data_augmentation,
+                output_transforms=(self.output_transforms +
+                                   self.data_augmentation),
                 label_transforms=self.labels_transforms,
                 patch_size=patch_size, input_size=input_size)
             val_dataset = ArrayDataset(
                 self.inputs, fold_val_index,
                 labels=self.labels, outputs=self.outputs,
-                add_input=self.add_input, input_transforms=self.input_transforms,
+                add_input=self.add_input,
+                input_transforms=self.input_transforms,
                 output_transforms=self.output_transforms,
                 label_transforms=self.labels_transforms,
                 patch_size=patch_size, input_size=input_size
             )
             self.dataset["train"].append(train_dataset)
             self.dataset["validation"].append(val_dataset)
-
 
     @staticmethod
     def get_indices_from_mask(mask):
@@ -248,10 +266,13 @@ class DataManager(object):
         """
         data = OrderedDict()
         for key in ("inputs", "outputs", "labels"):
-            if len(list_samples) == 0 or getattr(list_samples[-1], key) is None:
+            if (len(list_samples) == 0 or
+                    getattr(list_samples[-1], key) is None):
                 data[key] = None
             else:
-                data[key] = torch.stack([torch.as_tensor(getattr(s, key)) for s in list_samples], dim=0).float()
+                data[key] = torch.stack([
+                    torch.as_tensor(getattr(s, key))
+                    for s in list_samples], dim=0).float()
         if data["labels"] is not None:
             data["labels"] = data["labels"].type(torch.FloatTensor)
         return DataItem(**data)
@@ -284,11 +305,14 @@ class DataManager(object):
         if train:
             if self.weighted_random_sampler:
                 indices = self.dataset["train"][fold_index].indices
-                samples_weigths = self.sampler_weigths[np.array(self.stratify_label[indices], dtype=np.int32)]
-                sampler = WeightedRandomSampler(samples_weigths, len(indices), replacement=True)
+                samples_weigths = self.sampler_weigths[
+                    np.array(self.stratify_label[indices], dtype=np.int32)]
+                sampler = WeightedRandomSampler(
+                    samples_weigths, len(indices), replacement=True)
             _train = DataLoader(
-                self.dataset["train"][fold_index], batch_size=self.batch_size, sampler=sampler,
-                collate_fn=self.collate_fn, **self.data_loader_kwargs)
+                self.dataset["train"][fold_index], batch_size=self.batch_size,
+                sampler=sampler, collate_fn=self.collate_fn,
+                **self.data_loader_kwargs)
         if validation:
             _validation = DataLoader(
                 self.dataset["validation"][fold_index],
@@ -328,8 +352,9 @@ class ArrayDataset(Dataset):
     """ A dataset based on numpy array.
     """
     def __init__(self, inputs, indices, labels=None, outputs=None,
-                 add_input=False, input_transforms=None, output_transforms=None,
-                 label_transforms=None, patch_size=None, input_size=None):
+                 add_input=False, input_transforms=None
+                 output_transforms=None, label_transforms=None,
+                 patch_size=None, input_size=None):
         """ Initialize the class.
 
         Parameters
@@ -358,9 +383,13 @@ class ArrayDataset(Dataset):
         self.patch_size = patch_size
         self.input_size = input_size
         if self.patch_size is not None:
-            assert np.array(self.patch_size).shape == np.array(self.input_size).shape
-            self.nb_patches_by_img = np.product(np.array(self.input_size) // np.array(self.patch_size))
-            self.input_cached, self.output_cached, self.label_cached, self.input_indx_cached = None, None, None, None
+            assert (np.array(self.patch_size).shape ==
+                    np.array(self.input_size).shape)
+            self.nb_patches_by_img = (
+                np.product(np.array(self.input_size) //
+                           np.array(self.patch_size)))
+            (self.input_cached, self.output_cached, self.label_cached,
+             self.input_indx_cached) = None, None, None, None
 
         self.input_transforms = input_transforms or []
         self.output_transforms = output_transforms or []
@@ -388,12 +417,15 @@ class ArrayDataset(Dataset):
             item = item // self.nb_patches_by_img
             if self.input_indx_cached == item:
                 # Retrieve directly the input (and eventually the output)
-                indx = np.unravel_index(offset, np.array(self.input_size) // np.array(self.patch_size))
+                indx = np.unravel_index(
+                    offset,
+                    np.array(self.input_size) // np.array(self.patch_size))
                 _inputs = self.input_cached[indx]
                 _outputs, _labels = self.output_cached, self.label_cached
                 if self.output_same_as_input:
                     _outputs = self.output_cached[indx]
-                return DataItem(inputs=_inputs, outputs=_outputs, labels=_labels)
+                return DataItem(inputs=_inputs, outputs=_outputs,
+                                labels=_labels)
 
         _inputs = self.inputs[self.indices[item]]
         _labels, _outputs = (None, None)
@@ -417,24 +449,26 @@ class ArrayDataset(Dataset):
             for tf in self.labels_transforms:
                 _labels = tf(_labels)
 
-        # Eventually, get only one patch of the input (and one patch of the corresponding output if add_input==True)
+        # Eventually, get only one patch of the input (and one patch of the
+        # corresponding output if add_input==True)
         if self.patch_size is not None:
             self.input_indx_cached = item
             from skimage.util.shape import view_as_blocks
             # from a flat index, convert it to an nd-array index
-            indx = np.unravel_index(offset, np.array(self.input_size) // np.array(self.patch_size))
+            indx = np.unravel_index(
+                offset, np.array(self.input_size) // np.array(self.patch_size))
 
             # Store everything in a cache to avoid useless computations...
             self.input_cached = view_as_blocks(_inputs, tuple(self.patch_size))
             self.output_cached = _outputs
             if self.output_same_as_input:
-                self.output_cached = view_as_blocks(_outputs, tuple(self.patch_size))
+                self.output_cached = view_as_blocks(
+                    _outputs, tuple(self.patch_size))
             self.label_cached = _labels
 
             _inputs = self.input_cached[indx]
             if self.output_same_as_input:
                 _outputs = self.output_cached[indx]
-
 
         return DataItem(inputs=_inputs, outputs=_outputs, labels=_labels)
 
