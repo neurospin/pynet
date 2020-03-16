c9125faa034f785d443f51b87b9b0c66cd10b496
Antoine Grigis <antoine.grigis@cea.fr>
2020-03-16 17:09:14 +0100

    pynet/tests/test_data_manager: increase test data size to allow the split.

--- pynet/tests/test_data_manager.py
+++ pynet/tests/test_data_manager.py
@@ -78,6 +78,7 @@ class TestDataManager(unittest.TestCase):
         kwargs["projection_labels"] = {
             "sex": "M"
         }
+        kwargs["test_size"] = 0.2
         kwargs["number_of_folds"] = 2
         batch_size = kwargs["batch_size"]
 
