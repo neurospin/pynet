28f95af6303698c3d583e4b74cd6e85c5b564b83
Antoine Grigis <antoine.grigis@cea.fr>
2020-03-19 15:41:20 +0100

    examples/genetic/genomic_prediction: add multi class MPL.

--- examples/genetic/genomic_prediction.py
+++ examples/genetic/genomic_prediction.py
@@ -147,6 +147,7 @@ class TwoLayersMLP(nn.Module):
             the dropout rate.
         """
         super(TwoLayersMLP, self).__init__()
+        self.nb_classes = nb_classes
         self.layers = nn.Sequential(collections.OrderedDict([
             ("linear1", nn.Linear(data_size, nb_neurons[0])),
             ("activation1", nn.ReLU()),
@@ -159,7 +160,8 @@ class TwoLayersMLP(nn.Module):
     def forward(self, x):
         layer1_out = self.layers[0](x)
         x = self.layers[1:](layer1_out)
-        x = x.view(x.size(0))
+        if self.nb_classes == 1:
+            x = x.view(x.size(0))
         return x, {"layer1": layer1_out}
 
 def linear1_l2_kernel_regularizer(signal):
@@ -176,48 +178,41 @@ def linear1_l1_activity_regularizer(signal):
     l1_regularization = lambda1 * torch.norm(layer1_out, 1)
     return l1_regularization
 
-if 0:
-    nb_snps = X_train.shape[1]
-    model = TwoLayersMLP(nb_snps, nb_neurons=[64, 32], nb_classes=1)
-    print(model)
-    cl = Classifier(
-        optimizer_name="SGD",
-        learning_rate=1e-4,
-        loss_name="MSELoss",
-        metrics=["pearson_correlation"],
-        model=model)
-    cl.add_observer("regularizer", linear1_l2_kernel_regularizer)
-    cl.add_observer("regularizer", linear1_l1_activity_regularizer)
-    test_history, train_history = cl.training(
-        manager=manager,
-        nb_epochs=100,
-        checkpointdir="/tmp/genomic_pred",
-        fold_index=0,
-        with_validation=True)
-    y_hat, X, y_true, loss, values = cl.testing(
-        manager=manager,
-        with_logit=False,
-        predict=False)
-    print(y_hat.shape, y_true.shape)
-    print(y_hat)
-    print(y_true)
-    print("MSE in prediction =", loss)
-    corr = np.corrcoef(y_true, y_hat)[0, 1]
-    print("Corr obs vs pred =", corr)
-    plt.figure()
-    plt.title("MLP: Observed vs Predicted Y")
-    plt.ylabel("Predicted")
-    plt.xlabel("Observed")
-    plt.scatter(y_test, y_hat, marker="o")
-
+nb_snps = X_train.shape[1]
+model = TwoLayersMLP(nb_snps, nb_neurons=[64, 32], nb_classes=1)
+print(model)
+cl = Classifier(
+    optimizer_name="SGD",
+    learning_rate=5e-4,
+    loss_name="MSELoss",
+    metrics=["pearson_correlation"],
+    model=model)
+cl.add_observer("regularizer", linear1_l2_kernel_regularizer)
+cl.add_observer("regularizer", linear1_l1_activity_regularizer)
+test_history, train_history = cl.training(
+    manager=manager,
+    nb_epochs=(100 if "CI_MODE" not in os.environ else 10),
+    checkpointdir="/tmp/genomic_pred",
+    fold_index=0,
+    with_validation=True)
+y_hat, X, y_true, loss, values = cl.testing(
+    manager=manager,
+    with_logit=False,
+    predict=False)
+print(y_hat.shape, y_true.shape)
+print(y_hat)
+print(y_true)
+print("MSE in prediction =", loss)
+corr = np.corrcoef(y_true, y_hat)[0, 1]
+print("Corr obs vs pred =", corr)
+plt.figure()
+plt.title("MLP: Observed vs Predicted Y")
+plt.ylabel("Predicted")
+plt.xlabel("Observed")
+plt.scatter(y_test, y_hat, marker="o")
 
 #############################################################################
-# Implements the same fully connected network (MLP) for a quantitative
-# target but in the context of multiclass target.
-
-
-#############################################################################
-# Implements the same probblem but witth a Convolutional Neural Network (CNN)
+# Implements the same probblem but with a Convolutional Neural Network (CNN)
 # for a quantitative target.
 
 
@@ -244,13 +239,13 @@ model = MyNet()
 print(model)
 cl = Classifier(
     optimizer_name="SGD",
-    learning_rate=1e-4,
+    learning_rate=5e-4,
     loss_name="MSELoss",
     metrics=["pearson_correlation"],
     model=model)
 test_history, train_history = cl.training(
     manager=manager,
-    nb_epochs=10,
+    nb_epochs=(50 if "CI_MODE" not in os.environ else 10),
     checkpointdir="/tmp/genomic_pred",
     fold_index=0,
     with_validation=True)
@@ -269,6 +264,75 @@ plt.title("MLP: Observed vs Predicted Y")
 plt.ylabel("Predicted")
 plt.xlabel("Observed")
 plt.scatter(y_test, y_hat, marker="o")
+
+#############################################################################
+# Implements the same fully connected network (MLP) for a quantitative
+# target but in the context of multiclass target.
+
+data = fetch_genomic_pred(
+    datasetdir="/tmp/genomic_pred",
+    to_categorical=True)
+manager = DataManager(
+    input_path=data.input_path,
+    labels=["env0_cat0", "env0_cat1", "env0_cat2"],
+    stratify_label="env0",
+    projection_labels={"env0": [0, 1, 2]},
+    metadata_path=data.metadata_path,
+    number_of_folds=2,
+    batch_size=5,
+    test_size=0.2)
+train_dataset = manager["train"][0]
+X_train = train_dataset.inputs[train_dataset.indices]
+y_train = train_dataset.labels[train_dataset.indices]
+test_dataset = manager["test"]
+X_test = test_dataset.inputs[test_dataset.indices]
+y_test = test_dataset.labels[test_dataset.indices]
+print(X_train.shape, y_train.shape)
+print(X_test.shape, y_test.shape)
+nb_snps = X_train.shape[1]
+y_train = manager["train"][0].labels[train_dataset.indices]
+print(y_train.shape)
+model = TwoLayersMLP(nb_snps, nb_neurons=[64, 32], nb_classes=3)
+print(model)
+def my_loss(x, y):
+    """ nn.CrossEntropyLoss expects a torch.LongTensor containing the class
+    indices without the channel dimension.
+    """
+    device = y.get_device()
+    y = torch.argmax(y, dim=1).type(torch.LongTensor)
+    if device != -1:
+        y = y.to(device)
+    criterion = nn.CrossEntropyLoss()
+    return criterion(x, y)
+cl = Classifier(
+    optimizer_name="Adam",
+    learning_rate=5e-4,
+    loss=my_loss,
+    model=model)
+test_history, train_history = cl.training(
+    manager=manager,
+    nb_epochs=(100 if "CI_MODE" not in os.environ else 10),
+    checkpointdir="/tmp/genomic_pred",
+    fold_index=0,
+    with_validation=True)
+y_hat, X, y_true, loss, values = cl.testing(
+    manager=manager,
+    with_logit=True,
+    predict=False)
+print(y_hat.shape, y_true.shape)
+print(y_hat)
+print(y_true)
+print("MSE in prediction =", loss)
+heat = np.zeros([3, 3])
+for i in range(3):
+    klass = np.nonzero(y_true[:, i] > 0)
+    for j in range(3):
+        heat[i, j] = np.mean(y_hat[klass, j])
+print("Probabilities matrix", heat)
+plt.figure()
+plot = plt.imshow(heat, cmap="Blues")
+plt.ylabel("Predicted class")
+plt.xlabel("Observed class")
 
 if "CI_MODE" not in os.environ:
     plt.show()
