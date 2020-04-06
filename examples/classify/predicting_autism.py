62f830b978109b52aa7965dec5a2bd46ae7e89af
Antoine Grigis <antoine.grigis@cea.fr>
2020-04-06 14:53:26 +0200

    examples/classify/predicting_autism: add roc curve.

--- examples/classify/predicting_autism.py
+++ examples/classify/predicting_autism.py
@@ -27,8 +27,9 @@ from pynet.datasets import DataManager
 from pynet.utils import setup_logging
 from pynet.plotting import Board, update_board
 from pynet.interfaces import DeepLearningInterface
-from sklearn.metrics import classification_report
 from pynet.metrics import SKMetrics
+from sklearn.metrics import classification_report
+from sklearn.metrics import roc_curve, auc
 import collections
 import torch
 import torch.nn as nn
@@ -208,5 +209,17 @@ metrics = pd.DataFrame.from_dict(metrics)
 print(classification_report(y_true, y_pred >= 0.4))
 print(metrics)
 # plot_metric_rank_correlations(metrics)
+fpr, tpr, _ = roc_curve(y_true, y_pred)
+roc_auc = auc(fpr, tpr)
+plt.figure()
+plt.plot(fpr, tpr, color="darkorange", lw=2,
+         label="ROC curve (area = %0.2f)" % roc_auc)
+plt.plot([0, 1], [0, 1], color="navy", lw=2, linestyle="--")
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.title("Receiver operating characteristic")
+plt.legend(loc="lower right")
 
 plt.show()
