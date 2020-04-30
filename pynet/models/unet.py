90d7bb34d91d383d1cf896d59010496d09bf8b5a
Antoine Grigis <antoine.grigis@cea.fr>
2020-04-30 13:26:04 +0200

    pynet/models/unet: add input shape to resize to scale shape during upsampling.

--- pynet/models/unet.py
+++ pynet/models/unet.py
@@ -13,12 +13,18 @@ The U-Net is a convolutional encoder-decoder neural network.
 
 # Imports
 import ast
+import logging
 import collections
 import torch
 import torch.nn as nn
 import torch.nn.functional as func
 from pynet.interfaces import DeepLearningDecorator
 from pynet.utils import Networks
+import numpy as np
+
+
+# Global parameters
+logger = logging.getLogger("pynet")
 
 
 @Networks.register
@@ -48,7 +54,8 @@ class UNet(nn.Module):
 
     def __init__(self, num_classes, in_channels=1, depth=5,
                  start_filts=64, up_mode="transpose",
-                 merge_mode="concat", batchnorm=False, dim="3d"):
+                 merge_mode="concat", batchnorm=False, dim="3d",
+                 input_shape=None):
         """ Init class.
 
         Parameters
@@ -70,6 +77,9 @@ class UNet(nn.Module):
             normalize the inputs of the activation function.
         dim: str, default '3d'
             '3d' or '2d' input data.
+        input_shape: uplet
+            the tensor data shape (X, Y, Z) used during upsample (by default
+            use a scale factor of 2).
         """
         # Inheritance
         nn.Module.__init__(self)
@@ -106,6 +116,11 @@ class UNet(nn.Module):
         self.depth = depth
         self.down = []
         self.up = []
+        self.shapes = None
+        if input_shape is not None:
+            self.shapes = self._downsample_shape(
+                input_shape, nb_iterations=(depth - 2))
+            self.shapes = self.shapes[::-1]
 
         # Create the encoder pathway
         for cnt in range(depth):
@@ -121,9 +136,12 @@ class UNet(nn.Module):
         for cnt in range(depth - 1):
             in_channels = out_channels
             out_channels = in_channels // 2
+            shape = None
+            if self.shapes is not None:
+                shape = self.shapes[cnt]
             self.up.append(
                 Up(in_channels, out_channels, up_mode=up_mode, dim=self.dim,
-                   merge_mode=merge_mode, batchnorm=batchnorm))
+                   merge_mode=merge_mode, batchnorm=batchnorm, shape=shape))
 
         # Add the list of modules to current module
         self.down = nn.ModuleList(self.down)
@@ -134,6 +152,14 @@ class UNet(nn.Module):
 
         # Kernel initializer
         self.kernel_initializer()
+
+    def _downsample_shape(self, shape, nb_iterations=1, scale_factor=2):
+        shape = np.asarray(shape)
+        all_shapes = [shape.astype(int).tolist()]
+        for idx in range(nb_iterations):
+            shape = np.floor(shape / scale_factor)
+            all_shapes.append(shape.astype(int).tolist())
+        return all_shapes
 
     def kernel_initializer(self):
         for module in self.modules():
@@ -146,19 +172,30 @@ class UNet(nn.Module):
             nn.init.constant_(module.bias, 0)
 
     def forward(self, x):
+        logger.debug("Unet...")
+        logger.debug("  input: {0} - {1} - {2}".format(
+            x.shape, x.get_device(), x.dtype))
         encoder_outs = []
         for module in self.down:
             x = module(x)
+            logger.debug("  down: {0} - {1} - {2}".format(
+                x.shape, x.get_device(), x.dtype))
             encoder_outs.append(x)
         encoder_outs = encoder_outs[:-1][::-1]
         for cnt, module in enumerate(self.up):
             x_up = encoder_outs[cnt]
+            logger.debug("  skip: {0} - {1}".format(
+                x.shape, x_up.shape))
             x = module(x, x_up)
+            logger.debug("  up: {0} - {1} - {2}".format(
+                x.shape, x.get_device(), x.dtype))
 
         # No softmax is used. This means you need to use
         # nn.CrossEntropyLoss in your training script,
         # as this module includes a softmax already.
         x = self.conv_final(x)
+        logger.debug("  final: {0} - {1} - {2}".format(
+            x.shape, x.get_device(), x.dtype))
         return x
 
 
@@ -196,16 +233,21 @@ class DoubleConv(nn.Module):
         return x
 
 
-def UpConv(in_channels, out_channels, dim, mode="transpose"):
+def UpConv(in_channels, out_channels, dim, mode="transpose", shape=None):
     if mode == "transpose":
         return eval(
             "nn.ConvTranspose{0}(in_channels, out_channels, kernel_size=2, "
             "stride=2)".format(dim))
     else:
         # out_channels is always going to be the same as in_channels
-        return nn.Sequential(collections.OrderedDict([
-            ("up", nn.Upsample(mode="nearest", scale_factor=2)),
-            ("conv1x", Conv1x1x1(in_channels, out_channels, dim))]))
+        if shape is None:
+            return nn.Sequential(collections.OrderedDict([
+                ("up", nn.Upsample(mode="nearest", scale_factor=2)),
+                ("conv1x", Conv1x1x1(in_channels, out_channels, dim))]))
+        else:
+            return nn.Sequential(collections.OrderedDict([
+                ("up", nn.Upsample(mode="nearest", size=shape)),
+                ("conv1x", Conv1x1x1(in_channels, out_channels, dim))]))
 
 
 def Conv1x1x1(in_channels, out_channels, dim, groups=1):
@@ -223,7 +265,6 @@ class Down(nn.Module):
                  batchnorm=True):
         super(Down, self).__init__()
         if pooling:
-
             self.ops = nn.Sequential(collections.OrderedDict([
                 ("maxpool", eval("nn.MaxPool{0}(2)".format(dim))),
                 ("doubleconv", DoubleConv(
@@ -244,10 +285,11 @@ class Up(nn.Module):
     A LeakyReLU activation and optionally a BatchNorm follows each convolution.
     """
     def __init__(self, in_channels, out_channels, dim, merge_mode="concat",
-                 up_mode="transpose", batchnorm=True):
+                 up_mode="transpose", batchnorm=True, shape=None):
         super(Up, self).__init__()
         self.merge_mode = merge_mode
-        self.upconv = UpConv(in_channels, out_channels, dim, mode=up_mode)
+        self.upconv = UpConv(
+            in_channels, out_channels, dim, shape=shape, mode=up_mode)
         self.doubleconv = DoubleConv(
             in_channels, out_channels, dim, batchnorm=batchnorm)
 
