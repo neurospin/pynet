89a820cc6c00f1d59a50350d4252a561fb5a7c12
Antoine Grigis <antoine.grigis@cea.fr>
2020-06-19 14:02:30 +0200

    pynet/datasets: add data manager helper.

--- pynet/datasets/__init__.py
+++ pynet/datasets/__init__.py
@@ -12,6 +12,8 @@ Module that privides common datasets.
 """
 
 
+import logging
+import numpy as np
 from pynet.utils import RegisteryDecorator
 
 
@@ -37,6 +39,10 @@ from .metastasis import fetch_metastasis
 from .toy import fetch_toy
 
 
+# Global parameters
+logger = logging.getLogger("pynet")
+
+
 def get_fetchers():
     """ Return all available data fetchers.
 
@@ -46,3 +52,76 @@ def get_fetchers():
         a dictionary containing all the fetchers.
     """
     return Fetchers.get_registry()
+
+
+def get_data_manager(fetcher_name, datasetdir, static_fold=0, slicevol=False,
+                     **kwargs):
+    """ Return a ready to use data manager.
+
+    Parameters
+    ----------
+    fetcher_name: str
+        the name of the fetcher to be used.
+    datasetdir: str
+        the dataset destination folder.
+    static_fold: int, default 0
+        access the Kth fold < 10.
+    slicevol: bool, default False
+        in case of a 3d dataset, slice the volume to get a 2d dataset.
+    kwargs: dict
+        pass the DataManager parameters.
+
+    Returns
+    -------
+    manager: DataManager
+        the ready to use data manager.
+    """
+    fetchers = get_fetchers()
+    fetcher = fetchers.get(fetcher_name)
+    if fetcher is None:
+        raise ValueError("Available fetchers are: {0}".format(
+            list(fetchers.keys())))
+    data = fetcher(datasetdir)
+    inputs = {}
+    if not hasattr(data, "metadata_path"):
+        raise ValueError("One metadata path is expected.")
+    if not hasattr(data, "input_path"):
+        raise ValueError("One input path is expected.")
+    if hasattr(data, "output_path"):
+        output_path = data.output_path
+    else:
+        output_path = None
+    if static_fold >= 10:
+        raise ValueError("Fold index must be lower than 10")
+    kwargs["number_of_folds"] = 10
+    manager = DataManager(
+        metadata_path=data.metadata_path, input_path=data.input_path,
+        output_path=output_path, **kwargs)
+    datasets = {
+        "train": manager["train"][static_fold],
+        "validation": manager["validation"][static_fold],
+        "test": manager["test"]}
+    if datasets["test"].outputs is None:
+        raise ValueError("This code does not support label outputs yet.")
+    inputs = {}
+    for key, dataset in datasets.items():
+        logger.debug("{0} indices: {1}".format(key, dataset.indices))
+        data = [dataset.inputs[dataset.indices],
+                dataset.outputs[dataset.indices]]
+        logger.debug("X: {0}".format(data[0].shape))
+        logger.debug("y: {0}".format(data[1].shape))
+        if slicevol:
+            for cnt, arr in enumerate(data):
+                assert arr.ndim == 5, "Expect a 3D volume for slicing"
+                shape = list(arr.shape[:-1])
+                shape[0] = -1
+                data[cnt] = arr.transpose(4, 0, 1, 2, 3).reshape(*shape)
+                logger.debug("slice volume: {0}".format(data[cnt].shape))
+        inputs["{0}_inputs".format(key)] = data[0]
+        inputs["{0}_outputs".format(key)] = data[1]
+    kwargs.update(inputs)
+    for name in ("test_size", "sample_size", "number_of_folds", "sampler"):
+        if name in kwargs:
+            kwargs.pop(name)
+    manager = DataManager.from_numpy(**kwargs)
+    return manager
