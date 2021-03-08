f935146c5ade0f93969d96cf411b52929fdacc48
Antoine Grigis <antoine.grigis@cea.fr>
2021-03-08 15:03:35 +0100

    pynet/datasets: update hbn and euaims fetchers.

--- pynet/datasets/hbn.py
+++ pynet/datasets/hbn.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ########################################################################
-# NSAp - Copyright (C) CEA, 2019
+# NSAp - Copyright (C) CEA, 2021
 # Distributed under the terms of the CeCILL-B license, as published by
 # the CEA-CNRS-INRIA. Refer to the LICENSE file or to
 # http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
@@ -22,51 +22,51 @@ logger = logging.getLogger("pynet")
 
 # Global parameters
 COHORT_NAME = "HBN"
-
-FOLDER = "/neurospin/brainomics/2020_deepint/data/"
-
-SAVING_FOLDER = "/neurospin/tmp/CA263211/preprocessed_data/HBN"
-
+FOLDER = "/neurospin/brainomics/2020_deepint/data"
+SAVING_FOLDER = "/tmp/HBN"
 FILES = {
-    "clinical": os.path.join(FOLDER, "HBN_clinical.tsv"),
+    "stratification": os.path.join(FOLDER, "HBN_stratification.tsv"),
     "rois_mapper": os.path.join(FOLDER, "HBN_rois.tsv"),
-    "clinical_rois": os.path.join(FOLDER, "HBN_stratification.tsv"),
-    "clinical_subgroups_full": os.path.join(FOLDER, "HBN_subgroups.tsv"),
-    "clinical_surface": os.path.join(FOLDER, "HBN_surf_stratification.tsv"),
-    "clinical_subgroups": os.path.join(FOLDER, "HBN_subgroups_angeline.csv"),
+    "surf_stratification": os.path.join(FOLDER, "HBN_surf_stratification.tsv"),
 }
-
 DEFAULTS = {
     "clinical": {
         "test_size": 0.2, "seed": 42,
         "return_data": False, "z_score": True,
-        "drop_cols": ["study site", "age", "sex", "wisc:fsiq", "mri", "euler"],
-        "qc": {"wisc:fsiq": {"gte": 70}, "euler": {"gt": -217},
-               "mri": {"eq": 1}},
+        "drop_cols": ["study site", "age", "sex", "wisc:fsiq", "mri",
+                      "euler", "labels", "subgroups"],
+        "qc": {"bloc-clinical_score-wisc:fsiq": {"gte": 70},
+               "bloc-clinical_score-euler": {"gt": -217},
+               "bloc-clinical_score-mri": {"eq": 1}}
     },
     "rois": {
-        "metrics": ["lgi:avg", "thick:avg", "surf:area"],
-        "roi_types": ["cortical"], "test_size": 0.2, "seed": 42,
+        "test_size": 0.2, "seed": 42,
         "return_data": False, "z_score": True, "adjust_sites": True,
+        "metrics": ["lgi:avg", "thick:avg", "surf:area"],
+        "roi_types": ["cortical"],
         "residualize_by": {"continuous": ["age", "wisc:fsiq"],
                            "discrete": ["sex"]},
-        "qc": {"wisc:fsiq": {"gte": 70}, "euler": {"gt": -217},
-               "mri": {"eq": 1}},
+        "qc": {"wisc:fsiq": {"gte": 70},
+               "euler": {"gt": -217},
+               "mri": {"eq": 1}}
     },
     "surface": {
+        "test_size": 0.2, "seed": 42,
+        "return_data": False, "z_score": True, "adjust_sites": True,
         "metrics": ["pial_lgi", "thickness"],
-        "test_size": 0.2, "seed": 42, "return_data": False,
-        "z_score": True, "adjust_sites": True,
-        "residualize_by": {"continuous": ["age", "wisc:fsiq"],
-                           "discrete": ["sex"]},
-        "qc": {"wisc:fsiq": {"gte": 70}, "euler": {"gt": -217},
-               "mri": {"eq": 1}},
+        "residualize_by": {"continuous": ["bloc-clinical_score-age",
+                                          "bloc-clinical_score-wisc:fsiq"],
+                           "discrete": ["bloc-clinical_score-sex"]},
+        "qc": {"bloc-clinical_score-wisc:fsiq": {"gte": 70},
+               "bloc-clinical_score-euler": {"gt": -217},
+               "bloc-clinical_score-mri": {"eq": 1}}
     },
     "multiblock": {
-        "blocks": ['clinical', 'surface-lh', 'surface-rh'],
         "test_size": 0.2, "seed": 42,
-        "qc": {"wisc:fsiq": {"gte": 70}, "euler": {"gt": -217},
-               "mri": {"eq": 1}},
+        "blocks": ["clinical", "surface-lh", "surface-rh"],
+        "qc": {"bloc-clinical_score-wisc:fsiq": {"gte": 70},
+               "bloc-clinical_score-euler": {"gt": -217},
+               "bloc-clinical_score-mri": {"eq": 1}}
     }
 }
 
@@ -74,28 +74,31 @@ DEFAULTS = {
 def make_fetchers(datasetdir=SAVING_FOLDER):
 
     return {
-        'clinical': WRAPPERS['clinical'](datasetdir=datasetdir, files=FILES,
-                                         cohort=COHORT_NAME,
-                                         subject_column_name="EID",
-                                         defaults=DEFAULTS['clinical']),
-        'rois': WRAPPERS['rois'](datasetdir=datasetdir, files=FILES,
-                                 cohort=COHORT_NAME,
-                                 site_column_name="study site",
-                                 defaults=DEFAULTS['rois']),
-        'surface-rh': WRAPPERS['surface'](hemisphere='rh', files=FILES,
-                                          datasetdir=datasetdir,
-                                          cohort=COHORT_NAME,
-                                          site_column_name="study site",
-                                          defaults=DEFAULTS['surface']),
-        'surface-lh': WRAPPERS['surface'](hemisphere='lh', files=FILES,
-                                          datasetdir=datasetdir,
-                                          cohort=COHORT_NAME,
-                                          site_column_name="study site",
-                                          defaults=DEFAULTS['surface']),
+        "clinical": WRAPPERS["clinical"](
+            datasetdir=datasetdir, files=FILES, cohort=COHORT_NAME,
+            defaults=DEFAULTS["clinical"]),
+        "rois": WRAPPERS["rois"](
+            datasetdir=datasetdir, files=FILES, cohort=COHORT_NAME,
+            site_column_name="study site", defaults=DEFAULTS["rois"]),
+        "surface-rh": WRAPPERS["surface"](
+            hemisphere="rh", files=FILES, datasetdir=datasetdir,
+            cohort=COHORT_NAME, defaults=DEFAULTS["surface"],
+            site_column_name="study site"),
+        "surface-lh": WRAPPERS["surface"](
+            hemisphere="lh", files=FILES, datasetdir=datasetdir,
+            cohort=COHORT_NAME, defaults=DEFAULTS["surface"],
+            site_column_name="study site")
     }
 
 
-fetch_multiblock_HBN = WRAPPERS["multiblock"](
-    datasetdir=SAVING_FOLDER, files=FILES,
-    cohort=COHORT_NAME, subject_column_name="EID",
-    defaults=DEFAULTS["multiblock"], make_fetchers_func=make_fetchers)
+def fetch_multiblock_hbn(datasetdir=SAVING_FOLDER, fetchers=make_fetchers,
+                         surface=False):
+    if surface:
+        DEFAULTS["multiblock"]["blocks"] = ["clinical", "surface-lh",
+                                            "surface-rh"]
+    else:
+        DEFAULTS["multiblock"]["blocks"] = ["clinical", "rois"]
+    return WRAPPERS["multiblock"](
+        datasetdir=datasetdir, files=FILES, cohort=COHORT_NAME,
+        subject_column_name="participant_id", defaults=DEFAULTS["multiblock"],
+        make_fetchers_func=make_fetchers)()
